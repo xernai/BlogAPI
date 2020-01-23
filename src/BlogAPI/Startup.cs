54de31ed48884b87dd6923d0663c91af53e729ed
xernai <montcorona@gmail.com>
2020-01-23 16:46:20 -0600

    Adding Client App with Angular

--- src/BlogAPI/Startup.cs
+++ src/BlogAPI/Startup.cs
@@ -13,6 +13,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using BlogAPI.Utilities;
+using Microsoft.AspNetCore.SpaServices.AngularCli;
 
 namespace BlogAPI
 {
@@ -28,14 +29,31 @@ namespace BlogAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
+
             string connectionString = Configuration.GetConnectionString("BlogPostsContext");
 
             services.AddDbContextPool<BlogPostsContext>(options => options.UseSqlServer(connectionString));   
-            services.AddControllers();
+    
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                    builder => builder.AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader());
+                      //  .AllowCredentials());
+            });
 
+            services.AddControllers();
             services.AddScoped(typeof(IDataRepository<>), typeof(DataRepository<>));
-             services.AddScoped(typeof(IRepository<>), typeof(Repository<,>));
+            services.AddScoped(typeof(IRepository<>), typeof(Repository<,>));
             services.AddScoped(typeof(IFileLogger), typeof(FileLogger));
+
+            // In production, the Angular files will be served from this directory
+            services.AddSpaStaticFiles(configuration =>
+            {
+                configuration.RootPath = "ClientApp/dist";
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,16 +63,42 @@ namespace BlogAPI
             {
                 app.UseDeveloperExceptionPage();
             }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
 
+            app.UseCors("CorsPolicy");
             app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseSpaStaticFiles();
+
+            // app.UseMvc(routes =>
+            // {
+            //    routes.MapRoute(
+            //        name: "default",
+            //      template: "{controller}/{action=Index}/{id?}");
+            // });
 
             app.UseRouting();
-
             app.UseAuthorization();
-
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllers();
+               endpoints.MapControllers();
+            });
+
+            app.UseSpa(spa =>
+            {
+               // To learn more about options for serving an Angular SPA from ASP.NET Core,
+               // see https://go.microsoft.com/fwlink/?linkid=864501
+
+               spa.Options.SourcePath = "ClientApp";
+
+               if (env.IsDevelopment())
+               {
+                   spa.UseAngularCliServer(npmScript: "start");
+               }
             });
         }
     }
