ae7f7901e7d915a50d660d2bb36ce87653729e27
xernai <montcorona@gmail.com>
2020-01-24 11:10:12 -0600

    Reinstall npm

--- src/BlogAPI/Startup.cs
+++ src/BlogAPI/Startup.cs
@@ -92,7 +92,6 @@ namespace BlogAPI
             {
                // To learn more about options for serving an Angular SPA from ASP.NET Core,
                // see https://go.microsoft.com/fwlink/?linkid=864501
-
                spa.Options.SourcePath = "ClientApp";
 
                if (env.IsDevelopment())
