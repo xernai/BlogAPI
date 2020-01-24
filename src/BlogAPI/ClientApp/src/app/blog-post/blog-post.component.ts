12e57a6e4e50d357497b7209cc5b89cf508df1c7
xernai <montcorona@gmail.com>
2020-01-24 16:30:14 -0600

    Updating Blog-post

--- src/BlogAPI/ClientApp/src/app/blog-post/blog-post.component.ts
+++ src/BlogAPI/ClientApp/src/app/blog-post/blog-post.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { BlogPostService } from '../services/blog-post.service';
+import { BlogPost } from '../models/blogpost';
 
 @Component({
   selector: 'app-blog-post',
@@ -6,10 +10,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./blog-post.component.scss']
 })
 export class BlogPostComponent implements OnInit {
+  blogPost$: Observable<BlogPost>;
+  postId: number;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private blogPostService: BlogPostService, private avRoute: ActivatedRoute) {
+    const idParam = 'id';
+    if (this.avRoute.snapshot.params[idParam]) {
+      this.postId = this.avRoute.snapshot.params[idParam];
+    }
   }
 
+  ngOnInit() {
+    this.loadBlogPost();
+  }
+
+  loadBlogPost() {
+    this.blogPost$ = this.blogPostService.getBlogPost(this.postId);
+  }
 }
